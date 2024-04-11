8be29d837b2e61b6f54c9ebd4f619e4eab2f2d07
Kayleen Sandoval <kayleen.sandoval02@gmail.com>
2024-04-10 22:34:36 -0500

    solved03

--- problems/03-ice-cream-shop.js
+++ problems/03-ice-cream-shop.js
@@ -5,19 +5,26 @@ user's favorite ice cream flavor. Recursively find out whether or not the shop
 offers their favorite flavor.
 
 Examples:
+***********************************************************************/
+
+
+function iceCreamShop(flavors, favorite) {
+  if (flavors.length === 0){
+    return false
+  }
+  if (flavors[0] === favorite){
+    return true
+
+  }
+  return iceCreamShop(flavors.slice(1), favorite)
+}
+
+debugger
 iceCreamShop(['vanilla', 'strawberry'], 'blue moon'); // false
 iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea'); // true
 iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio'); // false
 iceCreamShop(['moose tracks'], 'moose tracks'); // true
 iceCreamShop([], 'honey lavender'); // false
-***********************************************************************/
-
-
-function iceCreamShop(flavors, favorite) {
-  // Your code here 
-}
-
-
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = iceCreamShop;
